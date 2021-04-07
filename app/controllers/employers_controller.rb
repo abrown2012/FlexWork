a7b95e03bcd068334f357d1592c0cacddec1cc9d
Agnieszka Brown <abrown2012@icloud.com>
2021-04-07 10:28:50 -0400

    adds message why employer account was not created

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -10,6 +10,7 @@ class EmployersController < ApplicationController
             session[:employer_id] = @employer.id 
             redirect_to @employer, notice: "Your account was successfully created!"
         else 
+            flash.now[:alert] = @employer.errors.full_messages.to_sentence 
             render :new
         end 
     end 
