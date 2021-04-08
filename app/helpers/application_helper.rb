f41145f4e305d4fdb45db38053bb41475dca8a0a
Agnieszka Brown <abrown2012@icloud.com>
2021-04-07 21:41:45 -0400

    adds current_user method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def current_user
+        if session[:employer_id]
+            @current_user ||= User.find_by_id(session[:employer_id]) 
+        elsif session[:contractor_id]
+            @current_user ||= User.find_by_id(session[:contractor_id]) 
+        end 
+    end
+    
 end
