b1fd4872ee9b3c93f51d5fe8a69f3a86df10adad
Agnieszka Brown <abrown2012@icloud.com>
2021-04-20 23:18:42 -0400

    allows user to modify their account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def new
         @user = User.new
     end 
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
     end
 
     def create
-  
+        binding.pry
         @user = User.new(user_params)
         if @user.save 
             session[:user_id] = @user.id 
@@ -24,6 +25,24 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def edit 
+        @user = User.find(params[:id])
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        binding.pry
+        if current_user.id == @user.id 
+            if @user.update(user_params)
+                redirect_to user_path( @user ), notice: "Your account was updated"
+            else 
+                render :edit
+            end 
+        else
+            redirect_to user_path( @user ), notice: "You can only update your own account"
+        end
+    end 
 
     private
     def user_params 
